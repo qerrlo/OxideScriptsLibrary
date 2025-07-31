0cae2fceb66e26aa6bb0414dd2b2b22624f434c2
hinorium. <ldplayers620@gmail.com>
2025-08-01 02:31:37 +0900

    Update OurExecution.lua

--- Scripts/Games/OurExecution.lua
+++ Scripts/Games/OurExecution.lua
@@ -41,7 +41,7 @@ local VoiceChatService = cloneref(game:GetService("VoiceChatService"))
 local PlaceId, JobId = game.PlaceId, game.JobId
 local IsOnMobile = table.find({Enum.Platform.IOS, Enum.Platform.Android}, UserInputService:GetPlatform())
 
-local CurrentVersion = "1.0.2"
+local CurrentVersion = "1.0.3"
 
 local LocalPlayer = Players.LocalPlayer
 local PlayerGui = cloneref(LocalPlayer:FindFirstChildWhichIsA("PlayerGui"))
